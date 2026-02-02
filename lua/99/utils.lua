710c3453cd44e6165390a4b026a6db90f1c7ddfe
Cory Sandlin <7127557+corybsa@users.noreply.github.com>
2026-02-02 09:43:27 -0500

    Implement cross-platform path formatting function
    Added a function to format paths for cross-platform compatibility.

--- lua/99/utils.lua
+++ lua/99/utils.lua
@@ -1,11 +1,22 @@
+local path_separator = package.config:sub(1, 1)
+
 local M = {}
+
+--- Formats a path to work on all operating systems
+--- @param ... string Path components to join
+--- @return string
+local function format_path(...)
+  local parts = { ... }
+  return table.concat(parts, path_separator)
+end
+
 --- TODO: some people change their current working directory as they open new
 --- directories.  if this is still the case in neovim land, then we will need
 --- to make the _99_state have the project directory.
 --- @return string
 function M.random_file()
   return string.format(
-    "%s/tmp/99-%d",
+    format_path("%s", "tmp", "99-%d"),
     vim.uv.cwd(),
     math.floor(math.random() * 10000)
   )
