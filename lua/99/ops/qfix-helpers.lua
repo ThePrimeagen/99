11f5152b6423c353af4dcc19285dfee3f4a850fa
Cory Sandlin <sandlincb@upmc.edu>
2026-03-10 11:47:32 -0400

    switch back to non-greedy pattern

--- lua/99/ops/qfix-helpers.lua
+++ lua/99/ops/qfix-helpers.lua
@@ -2,7 +2,7 @@ local M = {}
 
 --- @return _99.Search.Result | nil
 function M.parse_line(line)
-  local filepath, lnum_raw, rest = line:match("^(.*):(%d+):(.+)$")
+  local filepath, lnum_raw, rest = line:match("^(.-):(%d+):(.+)$")
   if not filepath or not lnum_raw or not rest then
     return nil
   end
